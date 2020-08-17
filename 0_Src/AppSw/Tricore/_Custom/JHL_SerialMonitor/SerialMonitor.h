9bce4de899c70441d4ac3881b45f3c7e50522699
JanghooLee <dlwkdgn1@naver.com>
2020-08-17 17:49:46 +0900

    [dev] JHL_serialmonitor V2 개발1
    대폭 수정, 큐 기반 시스템 개발

--- 0_Src/AppSw/Tricore/_Custom/JHL_SerialMonitor/SerialMonitor.h
+++ 0_Src/AppSw/Tricore/_Custom/JHL_SerialMonitor/SerialMonitor.h
@@ -30,6 +30,7 @@ typedef struct {
     uint8                   *inputData;
     uint8                   *outputBuffer; // [64 + sizeof(Ifx_Fifo) + 8]; // size : [ASC_TX_BUFFER_SIZE + sizeof(Ifx_Fifo) + 8];
     uint8                   *inputBuffer; // [64 + sizeof(Ifx_Fifo) + 8]; // size : [ASC_RX_BUFFER_SIZE + sizeof(Ifx_Fifo) + 8];
+
 } _JHL_SerialMonitorConfig;
 
 typedef struct {
@@ -40,12 +41,18 @@ typedef struct {
     // 8 bytes have to be added to ensure a proper circular buffer handling independent from the address to which the buffers have been located.
 } JHL_SerialMonitorConfig;
 
+typedef struct {
+    uint8                   outputData[64];
+    uint8                   inputData[64];
+    uint8                   frontIdx;
+    uint8                   rearIdx;
+    Ifx_SizeT               count;
+} JHL_SerialMonitorDataStructure;
+
 typedef struct
 {
-    JHL_SerialMonitorConfig config;
-    Ifx_SizeT               count;
-    uint8                   ouputData[64];
-    uint8                   inputData[64];
+    JHL_SerialMonitorDataStructure  _ds;
+    JHL_SerialMonitorConfig         config;
 } JHL_SerialMonitor;
 
 /******************************************************************************/
@@ -61,11 +68,21 @@ IFX_EXTERN JHL_SerialMonitor g_SerialMonitor;
 /*-------------------------Function Prototypes--------------------------------*/
 /******************************************************************************/
 void JHL_SerialMonitorConfig_init(JHL_SerialMonitorConfig *config);
-void JHL_SerialMonitor_init(JHL_SerialMonitorConfig *config);
-// not implemented void _JHL_SerialMonitor_fullQueueException(); 
-// not implemented void _JHL_SerialMonitor_emptyQueueException();
+void JHL_SerialMonitor_init(JHL_SerialMonitor *monitor);
+// not implemented 
+void _JHL_SerialMonitor_fullQueueException();
+// not implemented 
+void _JHL_SerialMonitor_emptyQueueException();
+boolean _JHL_SerialMonitor_isFull();
+boolean _JHL_SerialMonitor_isEmpty();
+
 // not implemented void JHL_SerialMonitor_enQueue();
-// not implemented void JHL_SerialMonitor_deQeuue();
+uint8 JHL_SerialMonitor_currentQueueItemsCnt();
+
+// not implemented void JHL_SerialMonitor_enQueueText(char* str);
+boolean JHL_SerialMonitor_enQueueOneByteUnsignedInt(uint8 num);
+boolean JHL_SerialMonitor_deQeueueAndSendOneByteUnsignedInt();
+
 void JHL_SerialMonitor_tester();
 
 #endif
